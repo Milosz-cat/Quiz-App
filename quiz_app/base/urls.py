b3adf1d99442d166b486d77e265fc5e56d3eb0b3
Milosz Bochenek <miloszbochenek20@gmail.com>
2022-12-06 18:51:26 +0100

    Adding some styling

--- quiz_app/base/urls.py
+++ quiz_app/base/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',  views.home, name='home'),
     path('login/',  views.login, name='login'),
     path('register/',  views.register, name='register'),
-    path('category/',  views.category, name='category'),
+    path('',  views.category, name='category'),
     path('question/',  views.question, name='question'),
     path('start/',  views.start, name='start'),
 ]
