44b41028dad0b3a38aeb369c06fd43e7edc0e321
Milosz Bochenek <miloszbochenek20@gmail.com>
2023-03-02 23:57:46 +0100

    pseudo pagination

--- quiz_app/base/views.py
+++ quiz_app/base/views.py
@@ -9,16 +9,22 @@ from django.forms import formset_factory
 from .models import Quiz, Question, Answer
 
 from django.views.generic import DetailView
-from django.core.paginator import Paginator
+
+class QuizDetailView(DetailView):
+
+    model = Quiz
+    template_name = 'base/start_quiz.html'
+    context_object_name = 'quiz'
 
 def sing_up(request):
 
     if request.method == "POST":
-        #username = request.POST.get("username")
+
         username = request.POST['username']
         first_name = request.POST['fname']
         last_name = request.POST['lname']
         email = request.POST['email']
+        # TODO POROWNYWANIE HASEL
         password_1 = request.POST['pass1']
         password_2 = request.POST['pass2']
 
@@ -53,16 +59,18 @@ def sing_in(request):
 
     return render(request, 'base/sing_in.html')
 
+
 def sing_out(request):
    logout(request)
    messages.success(request, "Logged Out Successfully!")
    return redirect('sing_in')
 
+
 def home(request):
 
     quizzes = Quiz.objects.all()
-
     return render(request, 'base/home.html', {'quizzes': quizzes})
+
 
 def create_quiz(request):  
 
@@ -78,7 +86,6 @@ def create_quiz(request):
     
     context = {'form': form, 'title': title}
     return render(request, 'base/create.html', context)
-
 
 
 def create_question(request, quiz_id):
@@ -114,31 +121,22 @@ def create_question(request, quiz_id):
     context = {'form': form, 'formset': formset, 'title': title}
     return render(request, 'base/create.html', context)
 
-class QuizDetailView(DetailView):
-
-    model = Quiz
-    template_name = 'base/start_quiz.html'
-    context_object_name = 'quiz'
-
-
-
 
 def question(request, quiz_id):
 
     Current_Quiz = Quiz.objects.get(id=quiz_id)
     Questions = Question.objects.filter(quiz=Current_Quiz)
-        
-    Answers = Answer.objects.all()
-    list = []
 
+    Answers = Answer.objects.all()
     AnswerFormSet = formset_factory(Answer_Select_Form, extra=len(Questions))
     formset = AnswerFormSet()
-
+    
+    list = [] 
     for i, form in enumerate(formset.forms):
         form.fields['answers'].queryset = Answers.filter(question=Questions[i])
         list.append((form, Questions[i]))
 
-    context = {'quiz_id':quiz_id, 'list': list}
+    context = {'quiz_id':quiz_id, 'list': list, 'Questions': Questions}
     return render(request, 'base/question.html', context)
 
 def summary(request, quiz_id):
@@ -146,7 +144,6 @@ def summary(request, quiz_id):
     points=0
 
     if request.method == 'POST' :
-        print(request.POST)
 
         Current_Quiz = Quiz.objects.get(id=quiz_id)
         Questions = Question.objects.filter(quiz=Current_Quiz)
@@ -156,11 +153,9 @@ def summary(request, quiz_id):
             all_answers = Answer.objects.filter(question=Questions[i])
             correct_answers = all_answers.filter(is_correct=1)
 
-
             for n in all_answers:
                 if (str(n.pk) in selected_answers and n in correct_answers) or (str(n.pk) not in selected_answers and n not in correct_answers):
                     points+=1
-    
     
     answers_count = len(Answer.objects.filter(question__quiz_id=quiz_id))
     percentages = round((points / answers_count) * 100)
